22b91a88e48991e32596e17087145db247254c12
Victor <you@example.com>
2015-05-09 14:09:32 +0200

    beginning, back to basics

--- restaurantApp/app/controllers/Application.java
+++ restaurantApp/app/controllers/Application.java
@@ -6,9 +6,17 @@ import play.mvc.*;
 import views.html.*;
 
 public class Application extends Controller {
+	
+	public static Result index(){
+		return ok(
+				stocks_gerant.render()
+				);
+	}
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result login() {
+	    return ok(
+	        login.render()
+	    );
+	}
 
 }
