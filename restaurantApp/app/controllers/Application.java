dbadb86c173169b53729e981fcbec2fd2996fb2c
Victor <you@example.com>
2015-05-19 15:05:33 +0200

    il faut se logger avec http://localhost:9000/login et apres on est rerooter vers la page d'entrée. prochaine étape se connecter avec la base de donnée

--- restaurantApp/app/controllers/Application.java
+++ restaurantApp/app/controllers/Application.java
@@ -1,22 +1,46 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import views.html.*;
+import static play.data.Form.form;
+import play.data.Form;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.login;
+import views.html.stocks_gerant;
 
 public class Application extends Controller {
+	
+	public static class Login {
+
+	    public String email;
+	    public String password;
+
+	}
+	
+	public static Result login() {
+        return ok(
+            login.render(form(Login.class))
+        );
+    }
+	
+	public static Result authenticate() {
+	    Form<Login> loginForm = form(Login.class).bindFromRequest();
+	    if (loginForm.hasErrors()) {
+	        return badRequest(login.render(loginForm));
+	    } else {
+	        session().clear();
+	        session("email", loginForm.get().email);
+	        return redirect(
+	            routes.Application.index()
+	        );
+	    }
+	}
+	
+	
 	
 	public static Result index(){
 		return ok(
 				stocks_gerant.render()
 				);
-	}
-
-	public static Result login() {
-	    return ok(
-	        login.render()
-	    );
 	}
 
 }
