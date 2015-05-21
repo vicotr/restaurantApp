2006b0db18a85bb4d79e7e0865025c18d4e08d21
rda <rdaugy1991@gmail.com>
2015-05-21 23:04:20 +0200

    ajout d'un nouvel utilisateur

--- restaurantApp/app/controllers/Application.java
+++ restaurantApp/app/controllers/Application.java
@@ -5,10 +5,14 @@ import static play.data.Form.form;
 import com.avaje.ebean.Ebean;
 
 import models.Demande;
+import models.Fonction;
+import models.Utilisateur;
 import models.queries.userLog;
 import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.gerant_administration;
+import views.html.gerant_nouveau_membre;
 import views.html.login;
 import views.html.stocks_gerant;
 import views.html.gerant_demande_reapprovisionnement;
@@ -27,9 +31,49 @@ public class Application extends Controller {
 		public String commentaires;
 	}
 	
+	public static class NouvelUtilisateur{
+		public String identifiant;
+		public String password;
+		public String role;
+	}
+	
 	public static Result demande(){
 		return ok(
 				gerant_demande_reapprovisionnement.render()
+				);
+	}
+	
+	public static Result administration() {
+		return ok(
+				gerant_administration.render()
+				);
+	}
+
+	public static Result nouveauMembre() {
+		return ok(
+				gerant_nouveau_membre.render()
+				);
+	}
+	
+	public static Result nouveauMembreTraitement() {
+		Form<NouvelUtilisateur> nouvelUtilisateurForm = form(NouvelUtilisateur.class).bindFromRequest();
+		String pseudo = nouvelUtilisateurForm.get().identifiant;
+		String password = nouvelUtilisateurForm.get().password;
+		String role = nouvelUtilisateurForm.get().role;
+		
+		Fonction fonction = new Fonction();
+		fonction.setActivity(role);
+		
+		Utilisateur utilisateur = new Utilisateur();
+		utilisateur.setPseudo(pseudo);
+		utilisateur.setPassword(password);
+		utilisateur.setFonction(fonction);
+		
+		Ebean.save(fonction);
+		Ebean.save(utilisateur);
+		
+		return ok(
+				gerant_administration.render()
 				);
 	}
 	
